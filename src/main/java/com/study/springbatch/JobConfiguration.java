31fab82573e094591d4e1cee0a79256226f2b764
juhwan0815 <juwom0831@naver.com>
2023-01-12 22:22:33 +0900

    스프링 배치 - 스프링 배치 도메인 이해 : Job

--- src/main/java/com/study/springbatch/JobConfiguration.java
+++ src/main/java/com/study/springbatch/JobConfiguration.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class DBJobConfiguration {
+public class JobConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
