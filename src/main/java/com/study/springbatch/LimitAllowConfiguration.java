828e7f7391d35c3e7b07ef17309837cf6cd751eb
juhwan0815 <juwom0831@naver.com>
2023-01-15 22:48:12 +0900

    스프링 배치 - 스프링 배치 실행 - Step : startLimit() / allowStartIfComplete()

--- src/main/java/com/study/springbatch/LimitAllowConfiguration.java
+++ src/main/java/com/study/springbatch/LimitAllowConfiguration.java
@@ -4,12 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class TaskletConfiguration {
+public class LimitAllowConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -25,7 +21,6 @@ public class TaskletConfiguration {
     @Bean
     public Job job() {
         return jobBuilderFactory.get("job")
-                .incrementer(new RunIdIncrementer())
                 .start(step1())
                 .next(step2())
                 .build();
@@ -34,19 +29,22 @@ public class TaskletConfiguration {
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        return RepeatStatus.FINISHED;
-                    }
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("step1 was executed");
+                    return RepeatStatus.FINISHED;
                 })
+                .allowStartIfComplete(true)
                 .build();
     }
-
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet(new CustomTasklet())
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("step2 was executed");
+                    throw new RuntimeException("step2 has failed");
+//                    return RepeatStatus.FINISHED;
+                })
+                .startLimit(3)
                 .build();
     }
 }
