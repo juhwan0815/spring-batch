626327ec93a7860b3f3aa6b16ce458e18916f12b
juhwan0815 <juwom0831@naver.com>
2023-01-24 19:28:08 +0900

    스프링 배치 - 스프링 배치 청크 프로세스 활용 : JpaItemWriter

--- src/main/java/com/study/springbatch/JpaConfiguration.java
+++ src/main/java/com/study/springbatch/JpaConfiguration.java
@@ -6,29 +6,26 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
-import org.springframework.batch.item.json.JacksonJsonObjectMarshaller;
-import org.springframework.batch.item.json.builder.JsonFileItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
 import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManagerFactory;
 import java.util.Arrays;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class JdbcBatchConfiguration {
+public class JpaConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-    private final DataSource dataSource;
+    private final EntityManagerFactory entityManagerFactory;
 
     @Bean
     public Job job() {
@@ -42,25 +39,30 @@ public class JdbcBatchConfiguration {
         return stepBuilderFactory.get("step")
                 .<Customer, Customer>chunk(3)
                 .reader(customItemReader())
+                .processor(customItemProcessor())
                 .writer(customItemWriter())
                 .build();
     }
 
     @Bean
+    public ItemProcessor<Customer, Customer> customItemProcessor() {
+        return customer -> new Customer(customer.getUsername(), customer.getAge());
+    }
+
+    @Bean
     public ItemWriter<Customer> customItemWriter() {
-        return new JdbcBatchItemWriterBuilder<Customer>()
-                .dataSource(dataSource)
-                .sql("insert into customer values (:id, :age, :username)")
-                .beanMapped()
+        return new JpaItemWriterBuilder<Customer>()
+                .entityManagerFactory(entityManagerFactory)
+                .usePersist(true)
                 .build();
     }
 
     @Bean
     public ItemReader<Customer> customItemReader() {
         return new ListItemReader<>(Arrays.asList(
-                new Customer(11L, "황주환", 10),
-                new Customer(12L, "강태환", 10),
-                new Customer(13L, "황철원", 10)
+                new Customer(1L, "황주환", 10),
+                new Customer(2L, "강태환", 10),
+                new Customer(3L, "황철원", 10)
         ));
     }
 }
