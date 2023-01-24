6261e3050546fbef9257d0bbc24b25e92483f225
juhwan0815 <juwom0831@naver.com>
2023-01-24 18:33:17 +0900

    스프링 배치 - 스프링 배치 청크 프로세스 활용 : XML StaxEventItemWriter

--- src/main/java/com/study/springbatch/Customer.java
+++ src/main/java/com/study/springbatch/Customer.java
@@ -14,4 +14,5 @@ public class Customer {
     private String name;
 
     private Integer age;
+
 }
--- src/main/java/com/study/springbatch/XMLConfiguration.java
+++ src/main/java/com/study/springbatch/XMLConfiguration.java
@@ -8,34 +8,41 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.item.xml.builder.StaxEventItemWriterBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.oxm.Marshaller;
+import org.springframework.oxm.xstream.XStreamMarshaller;
 
+import javax.sql.DataSource;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class FlatFileFormattedConfiguration {
+public class XMLConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
+    private final DataSource dataSource;
+
     @Bean
     public Job job() {
         return jobBuilderFactory.get("job")
-                .start(step())
+                .start(step1())
                 .build();
     }
 
     @Bean
-    public Step step() {
-        return stepBuilderFactory.get("step1")
-                .<Customer, Customer>chunk(3)
+    public Step step1() {
+        return stepBuilderFactory.get("step")
+                .<Customer, Customer>chunk(10)
                 .reader(customItemReader())
                 .writer(customItemWriter())
                 .build();
@@ -43,16 +50,26 @@ public class FlatFileFormattedConfiguration {
 
     @Bean
     public ItemWriter<Customer> customItemWriter() {
-        return new FlatFileItemWriterBuilder<Customer>()
-                .name("flatFileWriter")
-                .resource(new FileSystemResource("/Users/juhwan/study/spring-batch/src/main/resources/customer.txt"))
-                .append(true)
-                .formatted()
-                .format("%-2d%-5s%-2d")
-                .names(new String[]{"id", "name", "age"})
+        return new StaxEventItemWriterBuilder<Customer>()
+                .name("staxEventWriter")
+                .marshaller(itemMarshaller())
+                .resource(new FileSystemResource("/Users/juhwan/study/spring-batch/src/main/resources/customer.xml"))
+                .rootTagName("customer")
                 .build();
     }
 
+    @Bean
+    public Marshaller itemMarshaller() {
+        Map<String, Class<?>> aliases = new HashMap<>();
+        aliases.put("customer", Customer.class);
+        aliases.put("id", Long.class);
+        aliases.put("name", String.class);
+        aliases.put("age", Integer.class);
+
+        XStreamMarshaller xStreamMarshaller = new XStreamMarshaller();
+        xStreamMarshaller.setAliases(aliases);
+        return xStreamMarshaller;
+    }
 
     @Bean
     public ItemReader<Customer> customItemReader() {
@@ -61,6 +78,7 @@ public class FlatFileFormattedConfiguration {
                 new Customer(2L, "강태환", 27),
                 new Customer(3L, "황철원", 27)
         );
+
         return new ListItemReader<>(customers);
     }
 
