a875876276c3ad904bf5a932f5e454c26c993635
juhwan0815 <juwom0831@naver.com>
2023-02-01 21:40:55 +0900

    스프링 배치 복습 - 스프링 배치 도메인 이해 : StepExecution

--- src/main/java/com/study/springbatch/StepExecutionConfiguration.java
+++ src/main/java/com/study/springbatch/StepExecutionConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class StepConfiguration {
+public class StepExecutionConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -24,6 +24,7 @@ public class StepConfiguration {
         return jobBuilderFactory.get("job")
                 .start(step1())
                 .next(step2())
+                .next(step3())
                 .build();
     }
 
@@ -40,7 +41,21 @@ public class StepConfiguration {
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet(new CustomTasklet())
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("step2 was executed");
+//                    throw new RuntimeException("step2 was failed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("step3 was executed");
+                    return RepeatStatus.FINISHED;
+                })
                 .build();
     }
 
