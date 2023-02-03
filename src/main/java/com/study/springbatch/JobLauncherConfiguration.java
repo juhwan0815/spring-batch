a1fbbd3f1ef80cbfa27700440f39bca6ab2828d6
juhwan0815 <juwom0831@naver.com>
2023-02-03 22:26:26 +0900

    스프링 배치 복습 - 스프링 배치 도메인 이해 : JobLauncher

--- src/main/java/com/study/springbatch/JobLauncherConfiguration.java
+++ src/main/java/com/study/springbatch/JobLauncherConfiguration.java
@@ -3,7 +3,6 @@ package com.study.springbatch;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -11,21 +10,17 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class ExecutionContextConfiguration {
+public class JobLauncherConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-    private final JobExecutionListener jobRepositoryListener;
-
     @Bean
     public Job job() {
         return jobBuilderFactory.get("job")
-                .listener(jobRepositoryListener)
                 .start(step1())
                 .next(step2())
                 .build();
@@ -35,6 +30,8 @@ public class ExecutionContextConfiguration {
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet((contribution, chunkContext) -> {
+                    Thread.sleep(3000);
+                    log.info("step1 was executed");
                     return RepeatStatus.FINISHED;
                 })
                 .build();
@@ -44,6 +41,8 @@ public class ExecutionContextConfiguration {
     public Step step2() {
         return stepBuilderFactory.get("step2")
                 .tasklet((contribution, chunkContext) -> {
+                    Thread.sleep(3000);
+                    log.info("step2 was executed");
                     return RepeatStatus.FINISHED;
                 })
                 .build();
---
+++ src/main/java/com/study/springbatch/Member.java
@@ -0,0 +1,13 @@
+package com.study.springbatch;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    private String id;
+}
