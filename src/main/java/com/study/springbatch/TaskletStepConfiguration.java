f83f61adbe9d62798e9c5e7d4aad4a6443218f35
juhwan0815 <juwom0831@naver.com>
2023-02-05 18:08:50 +0900

    스프링 배치 복습 - 스프링 배치 실행 - Step : TaskletStep - 개념 및 API 소개

---
+++ src/main/java/com/study/springbatch/TaskletStepConfiguration.java
@@ -0,0 +1,70 @@
+package com.study.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class TaskletStepConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return jobBuilderFactory.get("batchJob")
+                .incrementer(new RunIdIncrementer())
+                .start(taskletStep())
+                .next(chunkStep())
+                .build();
+    }
+
+    @Bean
+    public Step taskletStep() {
+        return stepBuilderFactory.get("taskletStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("taskletStep was executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step chunkStep() {
+        return stepBuilderFactory.get("chunkStep")
+                .<String, String>chunk(10)
+                .reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5")))
+                .processor(new ItemProcessor<String, String>() {
+                    @Override
+                    public String process(String item) throws Exception {
+                        return item.toUpperCase();
+                    }
+                })
+                .writer(new ItemWriter<String>() {
+                    @Override
+                    public void write(List<? extends String> items) throws Exception {
+                        items.forEach(item -> {
+                            log.info("item = {}", item);
+                        });
+                    }
+                })
+                .build();
+    }
+
+}
