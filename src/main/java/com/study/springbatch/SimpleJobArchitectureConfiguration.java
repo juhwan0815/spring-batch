e93cb1566621ce33d15b33b464e2b4507dd2ade7
juhwan0815 <juwom0831@naver.com>
2023-02-05 15:06:40 +0900

    스프링 배치 복습 - 스프링 배치 실행 - Job : SimpleJob 아키텍처

--- src/main/java/com/study/springbatch/SimpleJobArchitectureConfiguration.java
+++ src/main/java/com/study/springbatch/SimpleJobArchitectureConfiguration.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class IncrementerConfiguration {
+public class SimpleJobArchitectureConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
