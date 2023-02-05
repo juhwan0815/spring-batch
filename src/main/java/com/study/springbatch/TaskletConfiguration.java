3e385fe9c5942807c882923ed142320f433d8365
juhwan0815 <juwom0831@naver.com>
2023-02-05 18:43:09 +0900

    스프링 배치 복습 - 스프링 배치 실행 - Step : TaskletStep - startLimit() / allowStartIfComplete()

--- src/main/java/com/study/springbatch/TaskletConfiguration.java
+++ src/main/java/com/study/springbatch/TaskletConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,7 +22,6 @@ public class TaskletConfiguration {
     @Bean
     public Job job() {
         return jobBuilderFactory.get("job")
-                .incrementer(new RunIdIncrementer())
                 .start(step1())
                 .next(step2())
                 .build();
@@ -33,16 +31,20 @@ public class TaskletConfiguration {
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet((contribution, chunkContext) -> {
-                    log.info("step1 was executed");
                     return RepeatStatus.FINISHED;
                 })
+                .allowStartIfComplete(true)
                 .build();
     }
 
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet(new CustomTasklet())
+                .tasklet((contribution, chunkContext) -> {
+                    throw new RuntimeException("step2 was failed");
+//                    return RepeatStatus.FINISHED;
+                })
+                .startLimit(3)
                 .build();
     }
 
