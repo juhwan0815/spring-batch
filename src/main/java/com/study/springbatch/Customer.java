ee179c1d3b568dc892f998a7dad0049ff6806469
juhwan0815 <juwom0831@naver.com>
2023-02-13 00:18:58 +0900

    스프링 배치 복습 - 스프링 배치 청크 지향 프로세스 활용 - ItemReader : FlatFileItemReader - 개념 및 API 소개

---
+++ src/main/java/com/study/springbatch/Customer.java
@@ -0,0 +1,12 @@
+package com.study.springbatch;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+
+    private String name;
+    private int age;
+    private String year;
+
+}
--- src/main/java/com/study/springbatch/FlatFilesConfiguration.java
+++ src/main/java/com/study/springbatch/FlatFilesConfiguration.java
@@ -7,17 +7,17 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.core.io.ClassPathResource;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class ItemStreamConfiguration {
+public class FlatFilesConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -25,6 +25,7 @@ public class ItemStreamConfiguration {
     @Bean
     public Job job() {
         return jobBuilderFactory.get("job")
+                .incrementer(new RunIdIncrementer())
                 .start(step1())
                 .build();
     }
@@ -32,26 +33,25 @@ public class ItemStreamConfiguration {
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .<String, String>chunk(3)
+                .<Customer, Customer>chunk(5)
                 .reader(itemReader())
-                .writer(itemWriter())
+                .writer(items -> items.forEach(item -> log.info("item = {}", item)))
                 .build();
     }
 
     @Bean
-    public CustomItemStreamReader itemReader() {
-        List<String> items = new ArrayList<>(10);
+    public ItemReader<Customer> itemReader() {
 
-        for (int i = 0; i < 10; i++) {
-            items.add("item" + i);
-        }
+        FlatFileItemReader<Customer> itemReader = new FlatFileItemReader<>();
+        itemReader.setResource(new ClassPathResource("/customer.csv"));
 
-        return new CustomItemStreamReader(items);
-    }
+        DefaultLineMapper<Customer> lineMapper = new DefaultLineMapper<>();
+        lineMapper.setTokenizer(new DelimitedLineTokenizer());
+        lineMapper.setFieldSetMapper(new CustomerFieldSetMapper());
 
-    @Bean
-    public ItemWriter<String> itemWriter() {
-        return new CustomItemStreamWriter();
+        itemReader.setLineMapper(lineMapper);
+        itemReader.setLinesToSkip(1);
+        return itemReader;
     }
 
 }
