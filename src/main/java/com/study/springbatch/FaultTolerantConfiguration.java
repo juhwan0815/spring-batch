46c331c3f57d710900354dd8c983e3d2f9ac4fc0
juhwan0815 <juwom0831@naver.com>
2023-02-23 21:52:57 +0900

    스프링 배치 복습 - 스프링 배치 반복 및 오류 제어 : FaultTolerant

---
+++ src/main/java/com/study/springbatch/FaultTolerantConfiguration.java
@@ -0,0 +1,74 @@
+package com.study.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.*;
+import org.springframework.batch.repeat.exception.ExceptionHandler;
+import org.springframework.batch.repeat.exception.SimpleLimitExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class FaultTolerantConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    private int chunkSize = 5;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("job")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .<String, String>chunk(chunkSize)
+                .reader(new ItemReader<String>() {
+
+                    int i = 0;
+
+                    @Override
+                    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+                        i++;
+
+                        if (i == 1) {
+                            throw new IllegalArgumentException("this exception is skipped");
+                        }
+
+                        return i > 3 ? null : "item" + i;
+                    }
+                })
+                .processor(new ItemProcessor<String, String>() {
+                    @Override
+                    public String process(String item) throws Exception {
+                        throw new IllegalStateException("this exception is retried");
+                    }
+                })
+                .writer(items -> items.forEach(item -> log.info("item = {}", item)))
+                .faultTolerant()
+                .skip(IllegalArgumentException.class)
+                .skipLimit(2)
+                .retry(IllegalStateException.class)
+                .retryLimit(2)
+                .build();
+    }
+
+    @Bean
+    public ExceptionHandler exceptionHandler() {
+        return new SimpleLimitExceptionHandler(3);
+    }
+
+
+}
